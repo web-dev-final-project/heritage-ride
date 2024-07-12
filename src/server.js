eb60a6900a26dec3479e8b211fdb01389173d0a6
nuendo2004 <nuendo2004@gmail.com>
2024-07-12 08:39:25 -0400

    setup handlebars

--- src/server.js
+++ src/server.js
@@ -4,18 +4,26 @@ import express from "express";
 import { startLog, endLog } from "./utils/logger.js";
 import error from "./middleware/error.js";
 import "./utils/extend.js";
-import { SuccessResponse } from "./utils/utilClass.js";
 import initRouter from "./routes/index.js";
+import path from "path";
+import { handlebarsInstance } from "./settings.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.engine("handlebars", handlebarsInstance.engine);
+app.set("view engine", "handlebars");
+app.set("view", path.join(path.resolve(), "src/views"));
+
 app.listen(port, () => {
   logger.info(`Server has started on port ${port}...`);
 });
+
+app.use(express.static("public"));
 app.use(express.json());
 app.use(startLog);
 initRouter(app);
+
 app.get("/health", (req, res) => {
   res.send("healthy");
 });
