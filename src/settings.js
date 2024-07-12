afc016c00f31033d30120dd6143f0bfa8468be09
Wen, the_Producer <77943685+nuendo2004@users.noreply.github.com>
2024-07-12 13:08:47 -0400

    Add hbs templates (#3)
    * inital sav
    
    * setup handlebars
    
    * add basic handlebar templates
    
    * add handlebars temp, restructure routes

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,23 @@
-const db_url = process.env.DB_SERVER 
-const database = process.env.DB_NAME
+import exphbs from "express-handlebars";
+import path from "path";
+
+const db_url = process.env.DB_SERVER;
+const database = process.env.DB_NAME;
 
 export const mongoConfig = {
-    serverUrl: db_url,
-    database: database
+  serverUrl: db_url,
+  database: database,
 };
+
+export const handlebarsInstance = exphbs.create({
+  defaultLayout: "main",
+  layoutsDir: path.join(path.resolve(), "src/views/layouts"),
+  partialsDir: path.join(path.resolve(), "src/views/partials"),
+  helpers: {
+    asJSON: (obj, spacing) => {
+      if (typeof spacing === "number")
+        return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+      return new Handlebars.SafeString(JSON.stringify(obj));
+    },
+  },
+});
