e57b40c7d91e8cc60aa081ea05339eb5b94356a4
nuendo2004 <nuendo2004@gmail.com>
2024-07-12 21:04:25 -0400

    update 1

--- src/routes/users/userView.js
+++ src/routes/users/userView.js
@@ -1,47 +1,47 @@
-import { Router } from "express";
-import Validator from "../../utils/validator.js";
-import * as users from "../../data/users.js";
-import { NotFoundException } from "../../utils/exceptions.js";
-import auth from "../../middleware/auth.js";
-import { getApiRoutes } from "../index.js";
-import logger from "../../utils/logger.js";
+import { Router } from 'express'
+import Validator from '../../utils/validator.js'
+import * as users from '../../data/users.js'
+import { NotFoundException } from '../../utils/exceptions.js'
+import auth from '../../middleware/auth.js'
+import { getApiRoutes } from '../index.js'
+import logger from '../../utils/logger.js'
 
-const router = Router();
+const router = Router()
 
-router.get("/", auth, async (req, res, next) => {
-  logger.info(`new request: (${req.method}) to ${req.url}.`);
+router.get('/', auth, async (req, res, next) => {
+  logger.info(`new request: (${req.method}) to ${req.url}.`)
   try {
-    const id = Validator.validateId(req.user._id);
-    const user = await users.findUser(id);
-    if (!user) throw new NotFoundException(`user not found`);
-    res.render("userProfile", user);
+    const id = Validator.validateId(req.user._id)
+    const user = await users.findUser(id)
+    if (!user) throw new NotFoundException(`user not found`)
+    res.render('userProfile', user)
   } catch (e) {
-    next(e);
+    next(e)
   }
-});
+})
 
-router.get("/login", (req, res, next) => {
+router.get('/login', (req, res, next) => {
   try {
-    res.render("login", { url: getApiRoutes(req).userRoute + "/login" });
+    res.render('login', { url: getApiRoutes(req).userRoute + '/login' })
   } catch (e) {
-    next(e);
+    next(e)
   }
-});
+})
 
 const cloudinary = {
   cloudName: process.env.CLOUDINARY_NAME,
   presetName: process.env.CLOUDINARY_PRESET,
-};
+}
 
-router.get("/signup", (req, res, next) => {
+router.get('/signup', (req, res, next) => {
   try {
-    res.render("signup", {
-      signUpUrl: getApiRoutes(req).userRoute + "/signup",
+    res.render('signup', {
+      signUpUrl: getApiRoutes(req).userRoute + '/signup',
       cloudinary: cloudinary,
-    });
+    })
   } catch (e) {
-    next(e);
+    next(e)
   }
-});
+})
 
-export default router;
+export default router
