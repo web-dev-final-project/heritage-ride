e0e3d36fd91b89572ba2c5bbad4b8c3ffef7c686
nuendo2004 <nuendo2004@gmail.com>
2024-07-12 22:54:56 -0400

    added jwt token handle

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,31 +1,31 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-import { AuthenticationException } from "./exceptions.js";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+import { AuthenticationException } from './exceptions.js'
 
-const JWT_SECRET = process.env.SERVER_SECRET;
+const JWT_SECRET = process.env.SERVER_SECRET
 
 const generateToken = (user) => {
   return jwt.sign({ user: user }, JWT_SECRET, {
-    expiresIn: "1d",
-  });
-};
+    expiresIn: '1d',
+  })
+}
 
 const verifyToken = (token) => {
   return jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
-      throw new AuthenticationException("User is not logged in.");
+      throw new AuthenticationException('User is not logged in.')
     }
-    return decoded;
-  });
-};
+    return decoded
+  })
+}
 
 const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
-};
+  const salt = await bcrypt.genSalt(10)
+  return bcrypt.hash(password, salt)
+}
 
 const comparePassword = async (password, hashedPassword) => {
-  return bcrypt.compare(password, hashedPassword);
-};
+  return bcrypt.compare(password, hashedPassword)
+}
 
-export { generateToken, comparePassword, hashPassword, verifyToken };
+export { generateToken, comparePassword, hashPassword, verifyToken }
