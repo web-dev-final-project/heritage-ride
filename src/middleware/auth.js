00e8753cc7bb30e95daf460179381317efddf39e
nuendo2004 <nuendo2004@gmail.com>
2024-07-13 01:08:25 -0400

    fix bugs

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,19 +1,19 @@
-import { AuthenticationException } from "../utils/exceptions.js";
-import { verifyToken } from "../utils/auth.js";
+import { AuthenticationException } from '../utils/exceptions.js'
+import { verifyToken } from '../utils/auth.js'
 
 const auth = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.cookies.token
   if (!token) {
-    throw new AuthenticationException("User has not yet logged in.");
+    throw new AuthenticationException('User has not yet logged in.')
   }
-  const decode = verifyToken(token);
+  const decode = verifyToken(token)
   // @ts-ignore
   if (!decode) {
-    throw new AuthenticationException("Invalid Token.");
+    throw new AuthenticationException('Invalid Token.')
   }
   // @ts-ignore
-  req.user = decode.user;
-  next();
-};
+  req.user = decode.user
+  next()
+}
 
-export default auth;
+export default auth
