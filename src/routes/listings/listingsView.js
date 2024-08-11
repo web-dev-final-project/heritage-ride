963581b1a9e21c172e1cc410e5e3e3a1942a25a7
nuendo2004 <nuendo2004@gmail.com>
2024-08-10 20:55:09 -0400

    add listing view

--- src/routes/listings/listingsView.js
+++ src/routes/listings/listingsView.js
@@ -8,7 +8,8 @@ import { findUser } from "../../data/users.js";
 
 const router = Router();
 
-router.get("/search", authSafe, async (req, res, next) => { // car listings search, displays cars
+router.get("/search", authSafe, async (req, res, next) => {
+  // car listings search, displays cars
   const query = req.query;
   // Error check
   const errors = Validator.validateQuery(query);
@@ -16,7 +17,7 @@ router.get("/search", authSafe, async (req, res, next) => { // car listings sear
     return res.status(400).render("carSearch", {
       error: "Invalid search criteria: " + errors.join(", "),
       results: [], // Ensure no results are shown if there's an error
-      user: req.user
+      user: req.user,
     });
   }
 
@@ -29,34 +30,35 @@ router.get("/search", authSafe, async (req, res, next) => { // car listings sear
   }
 });
 
-router.get('/search/:listingId',authSafe, async (req, res, next) => {
+router.get("/:listingId", authSafe, async (req, res, next) => {
   const listingId = req.params.listingId;
   //console.log(listingId)
   try {
-      // Fetch the listing details from the Listings collection
-      const listingDetails = await getListingById(listingId);
-      //console.log(listingDetails)
-      // Fetch the listing details from the Listings collection using the carId
-      const carDetails = await getCarById(listingDetails.itemId.toString());
-      //console.log(carDetails)
-      const sellerDetails = await findUser(listingDetails.sellerId.toString())
-      if (sellerDetails && sellerDetails.password) {
-        delete sellerDetails.password; // delete password field from current object
-      }
+    // Fetch the listing details from the Listings collection
+    const listingDetails = await getListingById(listingId);
+    //console.log(listingDetails)
+    // Fetch the listing details from the Listings collection using the carId
+    const carDetails = await getCarById(listingDetails.itemId.toString());
+    //console.log(carDetails)
+    const sellerDetails = await findUser(listingDetails.sellerId.toString());
+    if (sellerDetails && sellerDetails.password) {
+      delete sellerDetails.password; // delete password field from current object
+    }
 
-      if (!carDetails || !listingDetails) {
-          return res.status(404).render('404', { message: 'Car or listing not found', user: req.user });
-      }
+    if (!carDetails || !listingDetails) {
+      return res
+        .status(404)
+        .render("404", { message: "Car or listing not found", user: req.user });
+    }
 
-      // Render the details page with car and listing information
-      res.render('listingDetails', {
-          car: carDetails,
-          listing: listingDetails,
-          user: req.user,
-          seller: sellerDetails
-      });
-  } 
-  catch (e) {
+    // Render the details page with car and listing information
+    res.render("listingDetails", {
+      car: carDetails,
+      listing: listingDetails,
+      user: req.user,
+      seller: sellerDetails,
+    });
+  } catch (e) {
     next(e);
   }
 });
