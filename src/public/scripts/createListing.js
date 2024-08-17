d755a33489ab72904c7314dc3342128eff875782
jacquescore <jgcorenhs2016@gmail.com>
2024-08-16 21:46:05 -0700

    bug fix isSeller

--- src/public/scripts/createListing.js
+++ src/public/scripts/createListing.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form'); // Select the form element
-    const errorContainer = document.getElementById('errorContainer'); // Container for error messages
+    const form = document.querySelector('form');
+    const errorContainer = document.getElementById('errorContainer');
   
     form.addEventListener('submit', async (event) => {
-      event.preventDefault(); // Prevent the default form submission
+      event.preventDefault();
       
-      // Collect form data
       const formData = new FormData(form);
   
-      // Extract individual fields
       const carId = formData.get('carId');
       const price = formData.get('price');
       const image = formData.get('image');
@@ -18,21 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
       const itemType = urlParams.get('itemtype');
   
       try {
-        // Field validation
+        // Field validations
         if (!carId) {
           throw new Error('Car ID cannot be empty.');
         }
-        
         const parsedPrice = Number(price);
         if (isNaN(parsedPrice) || parsedPrice <= 0) {
           throw new Error('Price must be a valid positive number.');
         }
-  
         if (typeof image !== 'string' || image.trim() === '' || !/^https?:\/\/.+/.test(image)) {
             throw new Error('Image URL must be a valid URL.');
         }
   
-        // Send form data using fetch
+        // Send form data
         const response = await fetch('/api/listings/create', {
             method: 'POST',
             headers: {
@@ -48,15 +44,12 @@ document.addEventListener('DOMContentLoaded', () => {
           });
   
         if (response.ok) {
-          // If the response is successful, redirect to the seller page
           document.location.replace('/seller');
         } else {
-          // Handle errors from the server
           const json = await response.json();
           throw new Error(json.content || 'An error occurred');
         }
       } catch (e) {
-        // Display the error message
         errorContainer.innerHTML = e.message;
         errorContainer.style.display = 'block';
       }
